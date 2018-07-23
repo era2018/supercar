ad8629eab5b0fe022e8dbce78711b6e578f6ae75
Cody Baker <Cody.Baker@era2018.onmicrosoft.com>
2018-07-23 14:46:14 -0400

    DB connection in Controller Constructor

--- src/main/java/com/era/supercar/Controller.java
+++ src/main/java/com/era/supercar/Controller.java
@@ -15,29 +15,39 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 class Controller
 {
-    @GetMapping("/events")
-    public List<Event> getEvents()
-    {
+
+	private Statement pstatement;
+
+	public Controller()
+	{
+		System.out.println("Controller construction");
         Properties props = new Properties();
         props.put("User", "ubdb");
         props.put("Password", "123456");
         Connection con = null;
 
-        ArrayList<Event> events = new ArrayList<Event>();
-
         try {
             con = DriverManager.getConnection("jdbc:sybase:Tds:10.171.1.252:2638/ubdatabase", props);
+			this.pstatement = con.createStatement();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+        }
+	}
+
+    @GetMapping("/events")
+    public List<Event> getEvents()
+    {
+		ArrayList<Event> events = new ArrayList<Event>();
+
+        try {
 			String queryString = "select * from EventLog";
-			Statement  pstatement = con.createStatement();
-			ResultSet rs = pstatement.executeQuery(queryString );
+			ResultSet rs = pstatement.executeQuery(queryString);
 			
 			while (rs.next()) {
-                //Event ev = new Event(rs.getLong("eventID"));
                 Event ev = new Event(rs.getInt("eventID"), rs.getString("carID"), rs.getString("logTime"));
 			    events.add(ev);
 			}
-			pstatement.close();
-			con.close();
             rs.close();
             
             return events;
@@ -49,43 +59,3 @@ class Controller
         return events;
     }
 }
-
-
-/*
-
-public Event pull() {
-		Properties props = new Properties();
-		props.put( "User", "ubdb" );
-		props.put( "Password", "123456" );
-		Connection con = null;
-
-		try {
-			System.out.println("connecting");
-			con = DriverManager.getConnection("jdbc:sybase:Tds:10.171.1.252:2638/ubdatabase", props);
-			System.out.println("connected");
-			String queryString = "select * from EventLog";
-			Statement  pstatement = con.createStatement();
-			ResultSet rs = pstatement.executeQuery(queryString );
-			
-			System.out.println("quried.");
-			
-			while (rs.next()) {
-				Event ev = new Event(rs.getLong("eventID"));
-				bag.addElement(ev);
-			}
-			pstatement.close();
-			con.close();
-			rs.close();
-
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
-		
-		return null;
-		
-		
-    } 
-    
-    */
